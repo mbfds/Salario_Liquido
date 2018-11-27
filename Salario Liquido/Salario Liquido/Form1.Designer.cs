3b6bd3a28796d9e2e10dd766f31216fa03615006
mbfds <marcelo.florentino.ti@gmail.com>
2018-11-27 12:17:33 -0200

    Trabalho desenvolvido no modulo de logica de programação

---
+++ Salario Liquido/Salario Liquido/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Salario_Liquido
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rbContruibuSocialSim = new System.Windows.Forms.RadioButton();
+            this.rbContruibuSocialNao = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtPlanSaude = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo de Salário Líquido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salário Bruto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contribuição Sindical :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Plano de Saúde";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Resultado:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // rbContruibuSocialSim
+            // 
+            this.rbContruibuSocialSim.AutoSize = true;
+            this.rbContruibuSocialSim.Location = new System.Drawing.Point(154, 140);
+            this.rbContruibuSocialSim.Name = "rbContruibuSocialSim";
+            this.rbContruibuSocialSim.Size = new System.Drawing.Size(42, 17);
+            this.rbContruibuSocialSim.TabIndex = 6;
+            this.rbContruibuSocialSim.TabStop = true;
+            this.rbContruibuSocialSim.Text = "Sim";
+            this.rbContruibuSocialSim.UseVisualStyleBackColor = true;
+            // 
+            // rbContruibuSocialNao
+            // 
+            this.rbContruibuSocialNao.AutoSize = true;
+            this.rbContruibuSocialNao.Location = new System.Drawing.Point(245, 140);
+            this.rbContruibuSocialNao.Name = "rbContruibuSocialNao";
+            this.rbContruibuSocialNao.Size = new System.Drawing.Size(45, 17);
+            this.rbContruibuSocialNao.TabIndex = 7;
+            this.rbContruibuSocialNao.TabStop = true;
+            this.rbContruibuSocialNao.Text = "Não";
+            this.rbContruibuSocialNao.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(70, 224);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(242, 224);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(90, 281);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(227, 111);
+            this.txtResultado.TabIndex = 10;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(102, 63);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(215, 20);
+            this.txtNome.TabIndex = 11;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(102, 100);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(215, 20);
+            this.txtSalario.TabIndex = 12;
+            // 
+            // txtPlanSaude
+            // 
+            this.txtPlanSaude.Location = new System.Drawing.Point(115, 185);
+            this.txtPlanSaude.Name = "txtPlanSaude";
+            this.txtPlanSaude.Size = new System.Drawing.Size(202, 20);
+            this.txtPlanSaude.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 450);
+            this.Controls.Add(this.txtPlanSaude);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rbContruibuSocialNao);
+            this.Controls.Add(this.rbContruibuSocialSim);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton rbContruibuSocialSim;
+        private System.Windows.Forms.RadioButton rbContruibuSocialNao;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtPlanSaude;
+    }
+}
+
---
+++ Salario Liquido/Salario Liquido/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Salario_Liquido
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtPlanSaude.Clear();
+            txtSalario.Clear();
+            txtResultado.Clear();
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            float bruto = float.Parse(txtSalario.Text);
+            float planoSaude = float.Parse(txtPlanSaude.Text);
+            float salario, salario2, salarioFinal;
+
+            if (bruto <= 1693.72)
+            {
+
+                salario = (bruto * 8 / 100);
+                if (rbContruibuSocialSim.Checked)​
+                {​
+                    salario2 = (bruto * 2 / 100);​
+                    salarioFinal = bruto - salario - salario2 - planoSaude;​
+                }​
+                else​
+                {​
+                    ​
+                    salarioFinal = bruto - salario - planoSaude;​
+                }​
+
+            }
+            else if (bruto < 2822.91)
+            {
+
+                salario = (bruto * 9 / 100);
+                if (rbContruibuSocialSim.Checked)​
+                {​
+                    salario2 = (bruto * 2 / 100);​
+                    salarioFinal = bruto - salario - salario2 - planoSaude;​
+                }​
+                else​
+                {​
+                    ​
+                    salarioFinal = bruto - salario - planoSaude;​
+                }​
+            }
+            else if (bruto < 5645.80)
+            {
+                salario = (bruto * 11 / 100);
+                if (rbContruibuSocialSim.Checked)​
+                {​
+                    salario2 = (bruto * 2 / 100);​
+                    salarioFinal = bruto - salario - salario2 - planoSaude;​
+                }​
+                else​
+                {​
+                    ​
+                    salarioFinal = bruto - salario - planoSaude;​
+                }​
+            }
+            else
+            {
+                bruto = (float)5645.80;
+
+                salario = (bruto * 11 / 100);
+                if (rbContruibuSocialSim.Checked)​
+                {​
+                    salario2 = (bruto * 2 / 100);​
+                    salarioFinal = bruto - salario - salario2 - planoSaude;​
+                }​
+                else​
+                {​
+                    ​
+                    salarioFinal = bruto - salario - planoSaude;​
+                }​
+
+            }
+
+
+
+            txtResultado.Text = txtNome.Text + " Seu salario final é de : " + Convert.ToString(salarioFinal);
+
+
+
+
+        }
+    }
+}
